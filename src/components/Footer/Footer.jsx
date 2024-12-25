4fee5f805b434900529938cc8d343b004d09c67b
elbaz11 <elbazm488@gmail.com>
2024-12-25 20:51:26 +0200

    newupdate

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -31,16 +31,16 @@ export default function Footer() {
       {/* Payment Partners */}
       <div className="flex items-center gap-4">
         <span className="text-gray-500 font-medium">Payment Partners</span>
-        <img className='w-20 cursor-pointer' src="/fresh-cart/Amazon_Pay-Logo.wine.svg" alt="Amazon Pay" />
-        <img className='w-20 cursor-pointer' src="/fresh-cart/American-Express-Color.png" alt="American Express" />
-        <img className='w-10 cursor-pointer' src="/fresh-cart/Mastercard-logo.svg.png" alt="MasterCard" />
-        <img className='w-20 cursor-pointer' src="/fresh-cart/PayPal_logo.svg.png" alt="PayPal" />
+        <img className='w-20 cursor-pointer' src={`${process.env.PUBLIC_URL}/Amazon_Pay-Logo.wine.svg`} alt="Amazon Pay" />
+        <img className='w-20 cursor-pointer' src={`${process.env.PUBLIC_URL}/American-Express-Color.png`} alt="American Express" />
+        <img className='w-10 cursor-pointer' src={`${process.env.PUBLIC_URL}/Mastercard-logo.svg.png" alt="MasterCard`} />
+        <img className='w-20 cursor-pointer' src={`${process.env.PUBLIC_URL}/PayPal_logo.svg.png" alt="PayPal`} />
       </div>
       {/* App Store Links */}
       <div className="flex items-center gap-4">
         <span className="text-gray-500 font-medium dark:text-gray-300">Get deliveries with FreshCart</span>
-        <img className='w-20' src="/fresh-cart/google-play-apple-text-logo-thumbnail.png" alt="App Store" />
-        <img className='w-20' src="/fresh-cart/google-thumbnail.png" alt="Google Play" />
+        <img className='w-20' src={`${process.env.PUBLIC_URL}/google-play-apple-text-logo-thumbnail.png`} alt="App Store" />
+        <img className='w-20' src={`${process.env.PUBLIC_URL}/fresh-cart/google-thumbnail.png" alt="Google Play`} />
       </div>
     </div>
   </div>
